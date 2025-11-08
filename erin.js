ccea03ba1b6d94e46d26e9e574bf48116fa8f94d
zemyonpto <yiyoushiye@gmail.com>
2025-11-08 22:43:59 +0900

    Help me

---
+++ erin.js
@@ -0,0 +1,42 @@
+window.onload = ()=>{
+  //ロード時にstyle作成
+  const style = document.createElement("style");
+style.textContent = `
+  .udefuri {
+    animation-name: beat;
+    animation-duration: 0.8s;
+    animation-iteration-count: infinite;
+    transform-origin: -1em center;
+    display: inline-block;
+    margin-left: 0.5em;
+  }
+
+  .call {
+    margin-left: 0.5em;
+  }
+
+  @keyframes beat {
+    0%   { transform: rotate(0deg); }
+    50%  { transform: rotate(90deg); }
+    100% { transform: rotate(0deg); }
+  }
+`;
+document.head.appendChild(style);
+
+};
+
+function createErin(px){
+  //<p><span>(ﾟ∀ﾟ)</span><span class ="udefuri">O</span><span class = "call">えーりん！えーりん！</span></p>
+  const erin = document.createElement("p");
+  erin.style.fontSize = px + "px";//サイズ設定
+  const hand = document.createElement("span");
+  const face = document.createElement("span");
+  const call = document.createElement("span");
+  face.textContent = "(ﾟ∀ﾟ)";
+  hand.textContent = "O";
+  hand.classList.add("udefuri");//クラス付与
+  call.textContent = "えーりん！えーりん！";
+  call.classList.add("call");
+  erin.append(face,hand,call);//子要素に
+  document.body.appendChild(erin);
+}
